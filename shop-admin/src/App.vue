9b74d270f0f8056615cbf11125700530cbff4de3
SmitchChina <13191616505@163.com>
2019-03-22 11:29:18 +0800

    搭建基础结构和实现了登录页面

---
+++ shop-admin/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="app-wrap">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+  .app-wrap{
+    height: 100%;
+  }
+</style>
---
+++ shop-admin/src/views/login/index.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="login-wrap">
+    <div class="login-container">
+      <el-form label-position="top" :rules="loginrules" ref="loginRuleForm" label-width="80px" :model="LoginFrom">
+        <el-form-item label="用户名"  prop='username'>
+          <el-input v-model="LoginFrom.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码"  prop='password'>
+          <el-input v-model="LoginFrom.password" type='password'></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button class="login-btn" type="primary" @click="handelLogin">立即登录</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'Longin',
+  data () {
+    return {
+      LoginFrom: {
+        username: '',
+        password: ''
+      },
+      loginrules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+
+  methods: {
+    async handelLogin () {
+      this.$refs.loginRuleForm.validate((valid) => {
+        if (valid) {
+          this.login()
+        } else {
+          console.log('登录失败 !!!')
+          return false
+        }
+      })
+    },
+    async login () {
+      const loginfrom = this.LoginFrom
+      const { data, meta } = (await axios.post('http://localhost:8888/api/private/v1/login', loginfrom)).data
+      console.log(data, meta)
+      if (meta.status === 200) {
+        this.$message({
+          message: '登录成功',
+          type: 'success'
+        })
+      } else {
+        this.$message.error(`登录失败: ${meta.msg}`)
+      }
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+.login-wrap {
+  height: 100%;
+  background-color: skyblue;
+  position: relative;
+}
+.login-container {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%,-50%);
+  background-color: #fff;
+  border-radius: 10px;
+  padding: 50px;
+}
+.login-btn {
+  width: 100%;
+}
+</style>
